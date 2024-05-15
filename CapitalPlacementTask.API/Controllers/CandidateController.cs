1bc24725eab978f2d5a31e8b18163e58395380d8
Casido1 <ugochukwu.anunihu@gmail.com>
2024-05-15 08:37:59 +0100

    Added CRUD endpoints to CandidateController

---
+++ CapitalPlacementTask.API/Controllers/CandidateController.cs
@@ -0,0 +1,43 @@
+﻿using CapitalPlacementTask.API.Models.DTOs;
+using CapitalPlacementTask.API.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CapitalPlacementTask.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidateController : ControllerBase
+    {
+        private readonly ICandidateService _candidateService;
+
+        public CandidateController(ICandidateService candidateService)
+        {
+            _candidateService = candidateService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCandidateAsync([FromBody] CandidateDto candidateDto)
+        {
+            var result = await _candidateService.Create(candidateDto);
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCandidatesAsync()
+        {
+            var result = await _candidateService.GetAllCandidates();
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCandidateByIdAsync([FromRoute] Guid id)
+        {
+            var result = await _candidateService.Delete(id);
+
+            return Ok(result);
+        }
+    }
+}
+
--- CapitalPlacementTask.Test/obj/Debug/net8.0/CapitalPlacementTask.Test.AssemblyInfo.cs
+++ CapitalPlacementTask.Test/obj/Debug/net8.0/CapitalPlacementTask.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapitalPlacementTask.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1607a2e11554a2d074021d2180970cf627c61ddc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapitalPlacementTask.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapitalPlacementTask.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
