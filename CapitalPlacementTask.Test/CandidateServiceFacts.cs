ff069a90a2dd3e6dab179258aba3938720d592c3
Casido1 <ugochukwu.anunihu@gmail.com>
2024-05-15 09:27:26 +0100

    Added some unit tests

---
+++ CapitalPlacementTask.Test/CandidateServiceFacts.cs
@@ -0,0 +1,186 @@
+﻿using AutoMapper;
+using CapitalPlacementTask.API.Data.Repository.Interface;
+using CapitalPlacementTask.API.Models.DTOs;
+using CapitalPlacementTask.API.Models.Entities;
+using CapitalPlacementTask.API.Services.Implementation;
+using FluentAssertions;
+using Moq;
+using System.Net;
+
+namespace CapitalPlacementTask.Test
+{
+    public class CandidateServiceFacts
+    {
+        private readonly CandidateService _sut;
+        private readonly Mock<ICandidateRepository> _candidateRepoMock = new Mock<ICandidateRepository>();
+        private readonly Mock<IMapper> _mapperMock = new Mock<IMapper>();
+        public CandidateServiceFacts()
+        {
+            _sut = new CandidateService(_candidateRepoMock.Object, _mapperMock.Object);
+        }
+
+        [Fact]
+        public async Task Delete_ShouldReturnNotFound_WhenCandidateDoesNotExist()
+        {
+            // Arrange
+            var candidateIdToDelete = Guid.NewGuid();
+
+            _candidateRepoMock.Setup(x => x.DeleteById(It.IsAny<Guid>())).ReturnsAsync(() => false);
+
+            // Act
+            var candidate = await _sut.Delete(candidateIdToDelete);
+
+            // Assert
+            candidate.Status.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task Delete_ShouldReturnNoContent_WhenCandidateIsSuccessfullyDeleted()
+        {
+            // Arrange
+            var candidateIdToDelete = Guid.NewGuid();
+
+            _candidateRepoMock.Setup(x => x.DeleteById(It.IsAny<Guid>())).ReturnsAsync(() => true);
+            _candidateRepoMock.Setup(x => x.SaveChangesAsync()).ReturnsAsync(() => true);
+
+            // Act
+            var candidate = await _sut.Delete(candidateIdToDelete);
+
+            // Assert
+            candidate.Status.Should().Be(HttpStatusCode.NoContent);
+        }
+
+        [Fact]
+        public async Task Delete_ShouldReturnBadRequest_WhenCandidateFailedToDelete()
+        {
+            // Arrange
+            var candidateIdToDelete = Guid.NewGuid();
+            var errorMessage = "Delete operation failed";
+
+            _candidateRepoMock.Setup(x => x.DeleteById(It.IsAny<Guid>())).ReturnsAsync(() => true);
+            _candidateRepoMock.Setup(x => x.SaveChangesAsync()).ReturnsAsync(() => false);
+
+            // Act
+            var candidate = await _sut.Delete(candidateIdToDelete);
+
+            // Assert
+            candidate.Status.Should().Be(HttpStatusCode.BadRequest);
+            candidate.ErrorMessage.Should().Be(errorMessage);
+        }
+
+        [Fact]
+        public async Task Create_ShouldCreateCandidate()
+        {
+            // Arrange
+            var programIdToAdd = Guid.NewGuid();
+            var CandidateIdToAdd = Guid.NewGuid();
+            var questionId = Guid.NewGuid();
+
+            var candidateToAdd = new Candidate
+            {
+                FirstName = "ade",
+                LastName = "leke",
+                Email = "ade.leke@gmail.com",
+                PhoneNumber = "08567840336",
+                Nationality = "Nigerian",
+                CurrentResidence = "Nigeria",
+                IdNumber = "7697",
+                DateOfBirth = new DateTime(1995, 7, 5),
+                Gender = "Male",
+                CandidateId = CandidateIdToAdd,
+                ProgramInfoId = programIdToAdd,
+                Answers = new List<Answer>
+                        {
+                            new Answer {QuestionId = questionId, Text = "Yes"}
+                        }
+            };
+
+            var candidateDto = new CandidateDto
+            {
+                FirstName = "ade",
+                LastName = "leke",
+                Email = "ade.leke@gmail.com",
+                PhoneNumber = "08567840336",
+                Nationality = "Nigerian",
+                CurrentResidence = "Nigeria",
+                IdNumber = "7697",
+                DateOfBirth = new DateTime(1995, 7, 5),
+                Gender = "Male",
+                CandidateId = CandidateIdToAdd,
+                ProgramInfoId = programIdToAdd,
+                Answers = new List<Answer>
+                        {
+                            new Answer {QuestionId = questionId, Text = "Yes"}
+                        }
+            };
+
+            _mapperMock.Setup(mapper => mapper.Map<Candidate>(candidateDto))
+                      .Returns(candidateToAdd);
+            _candidateRepoMock.Setup(x => x.SaveChangesAsync()).ReturnsAsync(() => true);
+
+            // Act
+            var candidate = await _sut.Create(candidateDto);
+
+            // Assert
+            candidate.Data.Id.Should().Be(CandidateIdToAdd);
+            candidate.Status.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Create_ShouldFailToCreateCandidate()
+        {
+            // Arrange
+            var programIdToAdd = Guid.NewGuid();
+            var CandidateIdToAdd = Guid.NewGuid();
+            var questionId = Guid.NewGuid();
+
+            var candidateToAdd = new Candidate
+            {
+                FirstName = "ade",
+                LastName = "leke",
+                Email = "ade.leke@gmail.com",
+                PhoneNumber = "08567840336",
+                Nationality = "Nigerian",
+                CurrentResidence = "Nigeria",
+                IdNumber = "7697",
+                DateOfBirth = new DateTime(1995, 7, 5),
+                Gender = "Male",
+                CandidateId = CandidateIdToAdd,
+                ProgramInfoId = programIdToAdd,
+                Answers = new List<Answer>
+                        {
+                            new Answer {QuestionId = questionId, Text = "Yes"}
+                        }
+            };
+
+            var candidateDto = new CandidateDto
+            {
+                FirstName = "ade",
+                LastName = "leke",
+                Email = "ade.leke@gmail.com",
+                PhoneNumber = "08567840336",
+                Nationality = "Nigerian",
+                CurrentResidence = "Nigeria",
+                IdNumber = "7697",
+                DateOfBirth = new DateTime(1995, 7, 5),
+                Gender = "Male",
+                CandidateId = CandidateIdToAdd,
+                ProgramInfoId = programIdToAdd,
+                Answers = new List<Answer>
+                        {
+                            new Answer {QuestionId = questionId, Text = "Yes"}
+                        }
+            };
+
+            _mapperMock.Setup(mapper => mapper.Map<Candidate>(candidateDto))
+                      .Returns(candidateToAdd);
+            _candidateRepoMock.Setup(x => x.SaveChangesAsync()).ReturnsAsync(() => false);
+
+            // Act
+            var candidate = await _sut.Create(candidateDto);
+
+            // Assert
+            candidate.Status.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
---
+++ CapitalPlacementTask.Test/ProgramServiceFacts.cs
@@ -0,0 +1,384 @@
+﻿using AutoMapper;
+using CapitalPlacementTask.API.Data.Repository.Interface;
+using CapitalPlacementTask.API.Models.DTOs;
+using CapitalPlacementTask.API.Models.Entities;
+using CapitalPlacementTask.API.Services.Implementation;
+using FluentAssertions;
+using Moq;
+using System.Net;
+
+namespace CapitalPlacementTask.Test
+{
+    public class ProgramServiceFacts
+    {
+        private readonly ProgramService _sut;
+        private readonly Mock<IProgramRepository> _programRepoMock = new Mock<IProgramRepository>();
+        private readonly Mock<IMapper> _mapperMock = new Mock<IMapper>();
+        public ProgramServiceFacts()
+        {
+            _sut = new ProgramService(_programRepoMock.Object, _mapperMock.Object);
+        }
+
+        [Fact]
+        public async Task GetProgramById_ShouldReturnProgram_WhenProgramExists()
+        {
+            // Arrange
+            var programIdToAdd = Guid.NewGuid();
+
+            var programToAdd = new ProgramInfo
+            {
+                ProgramInfoId = programIdToAdd,
+                Title = "Test Title",
+                Description = "Test Description",
+                Employer = new Employer
+                {
+                    FirstName = "first",
+                    LastName = "second",
+                    Email = "first.second@gmail.com",
+                    PhoneNumber = "08136964004",
+                    Nationality = "Nigerian",
+                    CurrentResidence = "Nigeria",
+                    IdNumber = "1234",
+                    DateOfBirth = new DateTime(1992, 7, 5),
+                    Gender = "Male",
+                    EmployerId = Guid.NewGuid(),
+                    ProgramInfoId = programIdToAdd
+                },
+                Questions = new List<Question>
+                {
+                    new Question
+                    {
+                        QuestionId = Guid.NewGuid(),
+                        ProgramInfoId = programIdToAdd,
+                        QuestionType = "YesNo",
+                        Text = "Are you a software engineer?",
+                        Choices = new List<string> { "Yes", "No" },
+                        MaxNumberOfChoices = 1
+                    }
+                }
+            };
+
+            var programDto = new ProgramInfoDto
+            {
+                ProgramInfoId = programIdToAdd,
+                Title = "Test Title",
+                Description = "Test Description",
+                Employer = new Employer
+                {
+                    FirstName = "first",
+                    LastName = "second",
+                    Email = "first.second@gmail.com",
+                    PhoneNumber = "08136964004",
+                    Nationality = "Nigerian",
+                    CurrentResidence = "Nigeria",
+                    IdNumber = "1234",
+                    DateOfBirth = new DateTime(1992, 7, 5),
+                    Gender = "Male",
+                    EmployerId = Guid.NewGuid(),
+                    ProgramInfoId = programIdToAdd
+                },
+                Questions = new List<Question>
+                {
+                    new Question
+                    {
+                        QuestionId = Guid.NewGuid(),
+                        ProgramInfoId = programIdToAdd,
+                        QuestionType = "YesNo",
+                        Text = "Are you a software engineer?",
+                        Choices = new List<string> { "Yes", "No" },
+                        MaxNumberOfChoices = 1
+                    }
+                }
+            };
+
+            _mapperMock.Setup(mapper => mapper.Map<ProgramInfoDto>(programToAdd))
+                      .Returns(programDto);
+            _programRepoMock.Setup(x => x.GetById(programIdToAdd)).ReturnsAsync(programToAdd);
+
+            // Act
+            var program = await _sut.GetProgramById(programIdToAdd);
+
+            // Assert
+            program.Data.ProgramInfoId.Should().Be(programIdToAdd);
+        }
+
+        [Fact]
+        public async Task GetProgramById_ShouldReturnNull_WhenProgramDoesNotExists()
+        {
+            // Arrange
+            var programIdToAdd = Guid.NewGuid();
+
+            _programRepoMock.Setup(x => x.GetById(It.IsAny<Guid>())).ReturnsAsync(() => null);
+
+            // Act
+            var program = await _sut.GetProgramById(programIdToAdd);
+
+            // Assert
+            program.Status.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task Delete_ShouldReturnNotFound_WhenProgramDoesNotExist()
+        {
+            // Arrange
+            var programIdToDelete = Guid.NewGuid();
+
+            _programRepoMock.Setup(x => x.DeleteById(It.IsAny<Guid>())).ReturnsAsync(() => false);
+
+            // Act
+            var program = await _sut.Delete(programIdToDelete);
+
+            // Assert
+            program.Status.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task Delete_ShouldReturnNoContent_WhenProgramIsSuccessfullyDeleted()
+        {
+            // Arrange
+            var programIdToDelete = Guid.NewGuid();
+
+            _programRepoMock.Setup(x => x.DeleteById(It.IsAny<Guid>())).ReturnsAsync(() => true);
+            _programRepoMock.Setup(x => x.SaveChangesAsync()).ReturnsAsync(() => true);
+
+            // Act
+            var program = await _sut.Delete(programIdToDelete);
+
+            // Assert
+            program.Status.Should().Be(HttpStatusCode.NoContent);
+        }
+
+        [Fact]
+        public async Task Delete_ShouldReturnBadRequest_WhenProgramFailedToDelete()
+        {
+            // Arrange
+            var programIdToDelete = Guid.NewGuid();
+            var errorMessage = "Delete operation failed";
+
+            _programRepoMock.Setup(x => x.DeleteById(It.IsAny<Guid>())).ReturnsAsync(() => true);
+            _programRepoMock.Setup(x => x.SaveChangesAsync()).ReturnsAsync(() => false);
+
+            // Act
+            var program = await _sut.Delete(programIdToDelete);
+
+            // Assert
+            program.Status.Should().Be(HttpStatusCode.BadRequest);
+            program.ErrorMessage.Should().Be(errorMessage);
+        }
+
+        [Fact]
+        public async Task Create_ShouldCreateProgram()
+        {
+            // Arrange
+            var programIdToAdd = Guid.NewGuid();
+
+            var programToAdd = new ProgramInfo
+            {
+                ProgramInfoId = programIdToAdd,
+                Title = "Test Title",
+                Description = "Test Description",
+                Employer = new Employer
+                {
+                    FirstName = "first",
+                    LastName = "second",
+                    Email = "first.second@gmail.com",
+                    PhoneNumber = "08136964004",
+                    Nationality = "Nigerian",
+                    CurrentResidence = "Nigeria",
+                    IdNumber = "1234",
+                    DateOfBirth = new DateTime(1992, 7, 5),
+                    Gender = "Male",
+                    EmployerId = Guid.NewGuid(),
+                    ProgramInfoId = programIdToAdd
+                },
+                Questions = new List<Question>
+                {
+                    new Question
+                    {
+                        QuestionId = Guid.NewGuid(),
+                        ProgramInfoId = programIdToAdd,
+                        QuestionType = "YesNo",
+                        Text = "Are you a software engineer?",
+                        Choices = new List<string> { "Yes", "No" },
+                        MaxNumberOfChoices = 1
+                    }
+                }
+            };
+
+            var programDto = new ProgramInfoDto
+            {
+                ProgramInfoId = programIdToAdd,
+                Title = "Test Title",
+                Description = "Test Description",
+                Employer = new Employer
+                {
+                    FirstName = "first",
+                    LastName = "second",
+                    Email = "first.second@gmail.com",
+                    PhoneNumber = "08136964004",
+                    Nationality = "Nigerian",
+                    CurrentResidence = "Nigeria",
+                    IdNumber = "1234",
+                    DateOfBirth = new DateTime(1992, 7, 5),
+                    Gender = "Male",
+                    EmployerId = Guid.NewGuid(),
+                    ProgramInfoId = programIdToAdd
+                },
+                Questions = new List<Question>
+                {
+                    new Question
+                    {
+                        QuestionId = Guid.NewGuid(),
+                        ProgramInfoId = programIdToAdd,
+                        QuestionType = "YesNo",
+                        Text = "Are you a software engineer?",
+                        Choices = new List<string> { "Yes", "No" },
+                        MaxNumberOfChoices = 1
+                    }
+                }
+            };
+
+            _mapperMock.Setup(mapper => mapper.Map<ProgramInfo>(programDto))
+                      .Returns(programToAdd);
+            _programRepoMock.Setup(x => x.SaveChangesAsync()).ReturnsAsync(() => true);
+
+            // Act
+            var program = await _sut.Create(programDto);
+
+            // Assert
+            program.Data.Id.Should().Be(programIdToAdd);
+            program.Status.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Create_ShouldFailToCreateProgram()
+        {
+            // Arrange
+            var programIdToAdd = Guid.NewGuid();
+
+            var programToAdd = new ProgramInfo
+            {
+                ProgramInfoId = programIdToAdd,
+                Title = "Test Title",
+                Description = "Test Description",
+                Employer = new Employer
+                {
+                    FirstName = "first",
+                    LastName = "second",
+                    Email = "first.second@gmail.com",
+                    PhoneNumber = "08136964004",
+                    Nationality = "Nigerian",
+                    CurrentResidence = "Nigeria",
+                    IdNumber = "1234",
+                    DateOfBirth = new DateTime(1992, 7, 5),
+                    Gender = "Male",
+                    EmployerId = Guid.NewGuid(),
+                    ProgramInfoId = programIdToAdd
+                },
+                Questions = new List<Question>
+                {
+                    new Question
+                    {
+                        QuestionId = Guid.NewGuid(),
+                        ProgramInfoId = programIdToAdd,
+                        QuestionType = "YesNo",
+                        Text = "Are you a software engineer?",
+                        Choices = new List<string> { "Yes", "No" },
+                        MaxNumberOfChoices = 1
+                    }
+                }
+            };
+
+            var programDto = new ProgramInfoDto
+            {
+                ProgramInfoId = programIdToAdd,
+                Title = "Test Title",
+                Description = "Test Description",
+                Employer = new Employer
+                {
+                    FirstName = "first",
+                    LastName = "second",
+                    Email = "first.second@gmail.com",
+                    PhoneNumber = "08136964004",
+                    Nationality = "Nigerian",
+                    CurrentResidence = "Nigeria",
+                    IdNumber = "1234",
+                    DateOfBirth = new DateTime(1992, 7, 5),
+                    Gender = "Male",
+                    EmployerId = Guid.NewGuid(),
+                    ProgramInfoId = programIdToAdd
+                },
+                Questions = new List<Question>
+                {
+                    new Question
+                    {
+                        QuestionId = Guid.NewGuid(),
+                        ProgramInfoId = programIdToAdd,
+                        QuestionType = "YesNo",
+                        Text = "Are you a software engineer?",
+                        Choices = new List<string> { "Yes", "No" },
+                        MaxNumberOfChoices = 1
+                    }
+                }
+            };
+
+            _mapperMock.Setup(mapper => mapper.Map<ProgramInfo>(programDto))
+                      .Returns(programToAdd);
+            _programRepoMock.Setup(x => x.SaveChangesAsync()).ReturnsAsync(() => false);
+
+            // Act
+            var program = await _sut.Create(programDto);
+
+            // Assert
+            program.Status.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task Update_ShouldReturnNotFound_WhenProgramDoesNotExist()
+        {
+            // Arrange
+            var programIdToAdd = Guid.NewGuid();
+
+            var programDto = new ProgramInfoDto
+            {
+                ProgramInfoId = programIdToAdd,
+                Title = "Test Title",
+                Description = "Test Description",
+                Employer = new Employer
+                {
+                    FirstName = "first",
+                    LastName = "second",
+                    Email = "first.second@gmail.com",
+                    PhoneNumber = "08136964004",
+                    Nationality = "Nigerian",
+                    CurrentResidence = "Nigeria",
+                    IdNumber = "1234",
+                    DateOfBirth = new DateTime(1992, 7, 5),
+                    Gender = "Male",
+                    EmployerId = Guid.NewGuid(),
+                    ProgramInfoId = programIdToAdd
+                },
+                Questions = new List<Question>
+                {
+                    new Question
+                    {
+                        QuestionId = Guid.NewGuid(),
+                        ProgramInfoId = programIdToAdd,
+                        QuestionType = "YesNo",
+                        Text = "Are you a software engineer?",
+                        Choices = new List<string> { "Yes", "No" },
+                        MaxNumberOfChoices = 1
+                    }
+                }
+            };
+
+            _programRepoMock.Setup(x => x.GetById(It.IsAny<Guid>())).ReturnsAsync(() => null);
+
+            // Act
+            var program = await _sut.Update(programDto);
+
+            // Assert
+            program.Status.Should().Be(HttpStatusCode.NotFound);
+        }
+    }
+}
